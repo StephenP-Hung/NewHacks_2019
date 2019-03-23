9a9172264738ddb433d088dd062984e9fed5237e
StephenP-Hung <stephen.phung@mail.utoronto.ca>
2019-03-23 11:45:39 -0400

    Look at my previous commit message please.

---
+++ initial.py
@@ -0,0 +1,19 @@
+#Hello this is our initial file in Python.
+name = "lessgo"
+print("Ayo" + " " + name)
+
+
+niceyo = {"Hello":"dAdDEee",
+         4:["Nice", "Job", 1, 2, 4, "No you"],
+         "OOF": "GANG"}
+print(len(niceyo[4]))
+
+for i in niceyo:
+    print(niceyo[i])
+    if(niceyo[i] == "GANG"):
+        break
+
+def my_parents(parents_names):
+    print(parents_names + " don't love me")
+    
+my_parents("Yifei and his girl")
