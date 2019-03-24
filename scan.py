174956a9abb498b56413199be72064899c389563
StephenP-Hung <stephen.phung@mail.utoronto.ca>
2019-03-24 08:06:07 -0400

    Scan.py has been updated to allow interactive zooming

--- scan.py
+++ scan.py
@@ -2,17 +2,116 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-cap = cv.VideoCapture(0)
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame',gray)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
-# When everything done, release the capture
+# cap = cv.VideoCapture(0)
+# while(True):
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+#     # Our operations on the frame come here
+#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+#     # Display the resulting frame
+#     cv.imshow('frame',gray)
+#     if cv.waitKey(1) & 0xFF == ord('q'):
+#         break
+# # When everything done, release the capture
 
-cap.release()
-cv.destroyAllWindows()
+# cap.release()
+# cv.destroyAllWindows()
+
+
+count = 0
+cv2.namedWindow('frame')
+
+
+
+def zoom(centerX,centerY,flag):
+	scale=10
+	cam = cv2.VideoCapture(0)
+
+	while True:
+		ret_val, img = cam.read()
+		#get the webcam size
+		height, width, channels = img.shape
+		#prepare the crop
+		centerX,centerY=int(height/2),int(width/2)
+		radiusX,radiusY= int(scale*height/100),int(scale*width/100)
+
+		minX,maxX=centerX-radiusX,centerX+radiusX
+		minY,maxY=centerY-radiusY,centerY+radiusY
+		cropped = img[minX:maxX, minY:maxY]
+		resized_cropped = cv2.resize(cropped, (width, height))
+
+		cv2.imshow('frame', resized_cropped)
+
+		if cv2.waitKey(1) == 27:
+			break  # esc to quit
+
+      
+
+      	#add + or - 5 % to zoom
+
+        if (flag): 
+        	scale += 5  # +5
+
+        if (not flag and scale == 15): 
+            scale = 10  # normal scale
+
+        if(not flag):
+            scale = 5 # -5
+            print('zoom done')
+
+
+def zoom_on_press (event,x,y,flags,param):
+	if event == cv2.EVENT_LBUTTONUP:
+		#zoom in on
+		global count
+		if(count % 2 == 0):
+			click_flag = True
+			count += 1
+
+		else:
+			click_flag = False
+			count += 1
+
+		zoom(x,y,click_flag)
+		print('zoom on press done')
+	
+
+
+cv2.setMouseCallback('frame', zoom_on_press)
+
+def show_webcam():
+	print('start capture')
+	cap = cv2.VideoCapture(0)
+	while(True):
+		ret, frame = cap.read()
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		cv2.imshow('frame',gray)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+
+
+
+
+show_webcam()
+
+# 	print('show web done')
+
+# show_webcam()
+
+# import numpy as np
+# import cv2 as cv
+# cap = cv2.VideoCapture(0)
+# while(True):
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+#     # Our operations on the frame come here
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     # Display the resulting frame
+#     cv2.imshow('frame',gray)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+# # When everything done, release the capture
+# cap.release()
+# cv2.destroyAllWindows()
+
