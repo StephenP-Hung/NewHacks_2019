cd0c3d262159a04d864c145b0bf718566bfbd4ec
StephenP-Hung <stephen.phung@mail.utoronto.ca>
2019-03-24 09:26:52 -0400

    Revert "Merging"
    This reverts commit c85fe402e78d24845006a29f3a75c53c466edfda.

--- scan.py
+++ scan.py
@@ -33,15 +33,16 @@ def zoom(centerX,centerY,flag):
       
 
       	#add + or - 5 % to zoom
-		if (flag):
-			scale += 5  # +5
 
-		if (not flag and scale == 15): 
-			scale = 10  # normal scale
+        if (flag): 
+        	scale += 5  # +5
 
-		if(not flag):
-			scale = 5 # -5
-			print('zoom done')
+        if (not flag and scale == 15): 
+            scale = 10  # normal scale
+
+        if(not flag):
+            scale = 5 # -5
+            print('zoom done')
 
 
 def zoom_on_press (event,x,y,flags,param):
@@ -78,5 +79,6 @@ def show_webcam():
 
 
 show_webcam()
+
 
 
