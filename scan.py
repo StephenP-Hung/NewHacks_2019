d150fae2433f49fdd1223ca4b90707625c6ccced
StephenP-Hung <stephen.phung@mail.utoronto.ca>
2019-03-24 08:09:42 -0400

    Updated some headers and removed unnecessary files

--- scan.py
+++ scan.py
@@ -2,21 +2,6 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-# cap = cv.VideoCapture(0)
-# while(True):
-#     # Capture frame-by-frame
-#     ret, frame = cap.read()
-#     # Our operations on the frame come here
-#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-#     # Display the resulting frame
-#     cv.imshow('frame',gray)
-#     if cv.waitKey(1) & 0xFF == ord('q'):
-#         break
-# # When everything done, release the capture
-
-# cap.release()
-# cv.destroyAllWindows()
-
 
 count = 0
 cv2.namedWindow('frame')
@@ -95,23 +80,5 @@ def show_webcam():
 
 show_webcam()
 
-# 	print('show web done')
 
-# show_webcam()
-
-# import numpy as np
-# import cv2 as cv
-# cap = cv2.VideoCapture(0)
-# while(True):
-#     # Capture frame-by-frame
-#     ret, frame = cap.read()
-#     # Our operations on the frame come here
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     # Display the resulting frame
-#     cv2.imshow('frame',gray)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-# # When everything done, release the capture
-# cap.release()
-# cv2.destroyAllWindows()
 
